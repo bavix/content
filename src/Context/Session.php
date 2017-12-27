811cc46814a565ffac081f931e473f12367b5e13
Babichev Maxim <info@babichev.net>
2017-12-27 13:27:18 +0300

    add options

--- src/Context/Session.php
+++ src/Context/Session.php
@@ -9,10 +9,12 @@ class Session extends Container
      * Session constructor.
      *
      * @param string $password
+     * @param array  $options
      */
-    public function __construct(?string $password = null)
+    public function __construct(?string $password = null, array $options = [])
     {
-        \PHP_SAPI === 'cli' OR \session_id() OR \session_start();
+        \PHP_SAPI === 'cli' OR \session_id() OR \session_start($options);
+
         parent::__construct($password);
         $this->store = &$_SESSION;
     }
